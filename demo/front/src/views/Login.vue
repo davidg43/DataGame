1ff7584d760398693bda8a4fb79601c5bdca66cb
mikex2110 <migfier@gmail.com>
2023-06-23 12:44:53 +0200

    front

---
+++ demo/front/src/views/Login.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="container">
+    <!--    Logo Div-->
+    <div class="row">
+      <div class="col-12 text-center pt-3">
+        <router-link :to="{ name: 'Home' }">
+          <img id="logo" src="../assets/logo.png" />
+        </router-link>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12 justify-content-center d-flex flex-row pt-5">
+        <div id="signin-div" class="flex-item border">
+          <h2 class="pt-4 pl-4">Login</h2>
+          <form @submit="login" class="pt-4 pl-4 pr-4">
+            <div class="form-group">
+              <label>Email</label>
+              <input
+                type="email"
+                class="form-control"
+                v-model="email"
+                required
+              />
+            </div>
+            <div class="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                class="form-control"
+                v-model="password"
+                required
+              />
+            </div>
+            <small class="form-text text-muted"
+              >By continuing you are accepting our user conditios.</small
+            >
+            <button type="submit" class="btn btn-primary mt-2 py-0">
+              Continue
+              <div
+                v-if="loading"
+                class="spinner-border spinner-border-sm"
+                role="status"
+              >
+                <span class="sr-only">Loading...</span>
+              </div>
+            </button>
+          </form>
+          <hr />
+          <small class="form-text text-muted pt-2 pl-4 text-center"
+            >Not registered yet?</small
+          >
+          <p class="text-center">
+            <router-link
+              :to="{ name: 'Signup' }"
+              class="btn btn-dark text-center mx-auto px-5 py-1 mb-2"
+              >Create Your Account</router-link
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  props: ["baseURL"],
+  data() {
+    return {
+      email: null,
+      password: null,
+      loading: null,
+    };
+  },
+  methods: {
+    async login(e) {
+      e.preventDefault();
+      this.loading = true;
+
+      const user = {
+        email: this.email,
+        password: this.password,
+      };
+    }
+  },
+};
+</script>
+
+<style scoped>
+.btn-dark {
+  background-color: #e7e9ec;
+  color: #000;
+  font-size: smaller;
+  border-radius: 0;
+  border-color: #adb1b8 #a2a6ac #a2a6ac;
+}
+
+.btn-primary {
+  background-color: #f0c14b;
+  color: black;
+  border-color: #a88734 #9c7e31 #846a29;
+  border-radius: 0;
+}
+
+#logo {
+  width: 150px;
+}
+
+@media only screen and (min-width: 992px) {
+  #signin-div {
+    width: 40%;
+  }
+}
+</style>
