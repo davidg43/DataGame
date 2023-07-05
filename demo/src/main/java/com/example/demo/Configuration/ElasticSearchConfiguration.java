204fc3973a6e497fdf6db6b25d1738d004cc8676
davidg43 <david.gavira.43@gmail.com>
2023-07-05 12:24:03 +0200

    prueba v2

--- demo/src/main/java/com/example/demo/Configuration/ElasticSearchConfiguration.java
+++ demo/src/main/java/com/example/demo/Configuration/ElasticSearchConfiguration.java
@@ -1,5 +1,15 @@
 package com.example.demo.Configuration;
 
-public class ElasticSearchConfiguration {
-    
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ElasticSearchConfiguration extends ElasticsearchConfiguration{
+    @Override
+    @Bean
+    public ClientConfiguration clientConfiguration(){
+        return ClientConfiguration.builder().connectedToLocalhost().build();
+    }
 }
