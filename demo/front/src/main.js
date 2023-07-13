ea3752bb3dbb9f542d7775b7432877e8b66f253c
mikex2110 <migfier@gmail.com>
2023-07-13 11:19:48 +0200

    cambios

--- demo/front/src/main.js
+++ demo/front/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { router } from './routes';
+import { router } from './router/index.js';
 import './style.css'
 import 'flowbite';
 import App from './App.vue'
