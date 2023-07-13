3403e4a8f727569bfe18c1b6388931bce82a2d01
mikex2110 <migfier@gmail.com>
2023-07-13 12:09:30 +0200

    quitar llaves

--- demo/front/src/main.js
+++ demo/front/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { router } from './router/index.js';
+import  router  from './router/index.js';
 import './style.css'
 import 'flowbite';
 import App from './App.vue'
