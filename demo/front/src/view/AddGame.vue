d0a44a351cd27eeb400a330a3c9795f5222c6508
mikex2110 <65345891+mikex2110@users.noreply.github.com>
2023-08-02 11:16:14 +0200

    search

--- demo/front/src/view/AddGame.vue
+++ demo/front/src/view/AddGame.vue
@@ -1,3 +1,32 @@
+<script setup>
+import Layout from "./Layout.vue"; 
+
+function addGame(game) {
+  console.log(JSON.stringify(game));
+  fetch("http://localhost:8080/add/" + game, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(game)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    // Aquí puedes manejar la respuesta en caso de éxito si es necesario
+    // Por ejemplo, puedes mostrar un mensaje de éxito o redireccionar a otra página
+    console.log('Game edited successfully');
+  })
+  .catch(error => {
+    // Manejo de errores
+    console.error('Error editing game:', error);
+  });
+}
+
+</script>
+
+
 <template>
   <Layout>
     <div class="container">
@@ -44,9 +73,9 @@
           <label for="genres">Genres:</label>
           <input type="text" id="genres" v-model="newGame.genres" />
         </div>
-        <div class="add">
+        <div class="addGame">
           <router-link to="/games">
-            <button id="add" style= "width: 100%; height: 100%;">Add</button>
+            <button id="addGame" style= "width: 100%; height: 100%;">Add</button>
           </router-link>
         </div>
       </form>
@@ -55,48 +84,6 @@
     </div>
   </Layout>
 </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        newGame: {
-          title: "",
-          url: "",
-          released: "",
-          updated: "",
-          rating: "",
-          playTime: "",
-          achievements: "",
-          platforms: "",
-          developers: "",
-          genres: "",
-        },
-      };
-    },
-    methods: {
-      addGame() {
-        console.log('New Game:', this.newGame);
-        this.newGame = {
-          title: "",
-          url: "",
-          released: "",
-          updated: "",
-          rating: "",
-          playTime: "",
-          achievements: "",
-          platforms: "",
-          developers: "",
-          genres: "",
-        };
-      },
-    },
-  };
-  </script>
-
-<script setup>
-import Layout from "./Layout.vue"; // Adjust the path based on your project structure
-</script>
   
 <style>
 
@@ -124,7 +111,7 @@ import Layout from "./Layout.vue"; // Adjust the path based on your project stru
     color: #000000;
   }
   
-  .add {
+  .addGame {
     margin-top: 10px;
     height: 50px;
     text-align: center;
@@ -136,7 +123,7 @@ import Layout from "./Layout.vue"; // Adjust the path based on your project stru
 
   
  
-  #add:hover {
+  #addGame:hover {
     background-color: green;
     }
 
